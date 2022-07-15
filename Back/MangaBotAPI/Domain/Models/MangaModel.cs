229eb8c866f18b4f28b1402bbdc71460da8dba7d
Emrehliug <guigui410@gmail.com>
2022-07-15 17:10:59 -0300

    subindo camada domain com as models

---
+++ Back/MangaBotAPI/Domain/Models/MangaModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class MangaModel
+    {
+        private int mangaID;
+        public int MangaID
+        {
+            get
+            {
+                return mangaID;
+            }
+            set
+            {
+                mangaID = value;
+            }
+        }
+        public string Nome { get; set; }
+        public string Url { get; set; }
+        public int NumCapitulos { get; set; }
+    }
+}
---
+++ Back/MangaBotAPI/Domain/Models/UsuarioModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class UsuarioModel
+    {
+        private int usuarioID;
+        private string nome;
+        private string senha;
+        private string email;
+
+        public int UsuarioID
+        {
+            get
+            {
+                return usuarioID;
+            }
+            set
+            {
+                usuarioID = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Senha
+        {
+            get
+            {
+                return senha;
+            }
+            set
+            {
+                senha = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+    }
+}
