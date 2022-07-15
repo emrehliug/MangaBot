0be41a02cc7722bf38e34f46a3ff731865db3da9
emrehliug <guigui410@gmail.com>
2022-07-15 13:33:55 -0300

    iniciando o projeto/criando navbar

---
+++ Front/appMangabot/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarModule } from './nav-bar/nav-bar.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NavBarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
