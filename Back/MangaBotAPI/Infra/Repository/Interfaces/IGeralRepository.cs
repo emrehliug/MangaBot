e7b5c5b590f4163b3b5963f6d260122586378805
emrehliug <guigui410@gmail.com>
2022-07-18 15:20:51 -0300

    incluindo repository e service contextdb para o EF

---
+++ Back/MangaBotAPI/Infra/Repository/Interfaces/IGeralRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repository.Interfaces
+{
+    public interface IGeralRepository
+    {
+        void Adicionar<T>(T entity) where T : class;
+        void Atualizar<T>(T entity) where T : class;
+        void Deletar<T>(T entity) where T : class;
+
+    }
+}
--- Back/MangaBotAPI/MangaBotAPI/Program.cs
+++ Back/MangaBotAPI/MangaBotAPI/Program.cs
@@ -1,3 +1,6 @@
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MangaBotContext>(
+    contextdb => contextdb.UseMySQL(builder.Configuration.GetConnectionString("MySql"))
+    );
 
 var app = builder.Build();
 
